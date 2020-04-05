ee0f78939882e0826765da20515e02e1b92206a1
varun <varunswarup0@gmail.com>
2020-04-05 19:49:07 +0530

    useEffect & Dependcies

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const getStateFromLocalStorage = () => {
   const storage = localStorage.getItem('counterState');
@@ -12,7 +12,7 @@ const storeStateInLocalStorage = (state) => {
 };
 
 const Counter = ({ max, step }) => {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     setCount((c) => {
@@ -22,6 +22,10 @@ const Counter = ({ max, step }) => {
   };
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
+
+  useEffect(() => {
+    document.title = `Counter: ${count}`;
+  }, [count]);
 
   return (
     <div className="Counter">
